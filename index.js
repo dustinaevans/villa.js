6ca5dd0ab1f06e3746d58632a85e6c30bfd84369
Dustin Evans <dustinaevans@gmail.com>
2016-03-22 06:57:30 -0500

    Application construction
    Added application html structure and CSS. Built out express routes.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-var app = require('express');
+var express = require('express');
+var app = express();
+var admin = express();
+
+var Firebase = require('firebase');
+
+var firebaseRef = new Firebase('https://wptc.firebaseio.com/');
+
+var self = this;
+
+function getUsers() {
+    firebaseRef.child('users').on('value', function (snapshot) {
+        console.log(snapshot.val());
+    });
+};
+
+admin.use(express.static('admin'));
+
+app.use(express.static('app'));
+
+var client = app.listen(8001, function () {
+    var host = client.address().address
+    var port = client.address().port
+
+    console.log("Villa.js server listening at http: //%s:%s", host, port)
+});
+var admin = admin.listen(8002, function () {
+    var adminH = admin.address().address
+    var adminP = admin.address().port
+    console.log("Admin application listening at http://%s:%s", adminH, adminP);
+});
