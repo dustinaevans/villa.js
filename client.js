3b472ef515a3105fc2057dba924caa671b56d0af
Dustin Evans <dff3bc@gmail.com>
2016-03-23 17:16:58 -0500

    Created and integrated client

--- client.js
+++ client.js
@@ -2,9 +2,44 @@ var self = this;
 var express = require('express');
 var Firebase = require('firebase');
 var bodyParser = require('body-parser');
+var request = require('request');
 const EventEmiter = require('events');
 
 var app = express();
 var admin = express();
 var firebaseRef = new Firebase('https://wptc.firebaseio.com/');
 var myEmiter = new EventEmiter();
+
+var id = "0001";
+
+var state = {
+    address: id
+};
+
+var client = app.listen(8003, function () {
+    console.log("Client listening at 8003");
+});
+
+app.get('/power', function (req, res) {
+    res.send(state);
+});
+
+var checkIn = function () {
+    request({
+        url: 'http://192.168.0.139:8001/clients/power',
+        method: 'POST',
+        json: state
+    }, function (error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (response.statusCode != 404) {
+                console.log(response.statusCode + " checked in");
+            } else {
+                console.log(body);
+            }
+        }
+    });
+};
+
+setInterval(checkIn, 5000);
--- index.js
+++ index.js
@@ -10,16 +10,21 @@ var app = express();
 var admin = express();
 var firebaseRef = new Firebase('https://wptc.firebaseio.com/');
 var myEmiter = new EventEmiter();
+var clients = {};
 
 //static serve
 admin.use(express.static('admin'));
 app.use(express.static('app'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 
 
 //listeners
-var client = app.listen(8001, function () {
-    var host = client.address().address
-    var port = client.address().port
+var server = app.listen(8001, function () {
+    var host = server.address().address
+    var port = server.address().port
 
     console.log("Villa.js server listening at http: //%s:%s", host, port)
 });
@@ -36,13 +41,47 @@ app.get('/users', function (req, res) {
 });
 
 app.post('/users', function (req, res) {
-    myEmiter.emit('postEvent', req);
-    console.log(JSON.stringify(req.body));
+    myEmiter.emit('postEvent');
+    console.log(JSON.stringify(req.body, null, 2));
+    res.send(JSON.stringify(req.body, null, 2));
+});
+
+app.post('/clients/power', function (req, res) {
+    var clientObj = JSON.stringify(req.body, null, 2);
+    myEmiter.emit('clientAliveEvent', clientObj);
+    res.statusCode = 200;
+    res.send('ok');
 });
 
 
 // Events
-myEmiter.on('postEvent', (req) => {
+myEmiter.on('postEvent', () => {
     console.log("Post event called.");
 
 });
+
+myEmiter.on('clientAliveEvent', (clientObj) => {
+    console.log("client " + clientObj.address + " is alive.")
+    clients.address = clientObj.address;
+});
+
+////Load the request module
+//var request = require('request');
+//
+////Lets configure and request
+//request({
+//    url: 'https://modulus.io/contact/demo', //URL to hit
+//    qs: {from: 'blog example', time: +new Date()}, //Query string data
+//    method: 'POST',
+//    headers: {
+//        'Content-Type': 'MyContentType',
+//        'Custom-Header': 'Custom Value'
+//    },
+//    body: 'Hello Hello! String body!' //Set the body as a string
+//}, function(error, response, body){
+//    if(error) {
+//        console.log(error);
+//    } else {
+//        console.log(response.statusCode, body);
+//    }
+//});
